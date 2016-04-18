cacc76ca4d02f670f2ae595193f874b2f9eca7be
caosai <caosai@aliyun.com>
2016-04-18 17:39:49 +0800

    xxx

--- app/flasky.py
+++ app/flasky.py
@@ -6,8 +6,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'hello world'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return 'hello %s' % name
+
+
 if __name__ == '__main__':
     app.run(debug = True)
