ab35aa0815d409b43c854748a997f1aea6fcd0fb
caosai <caosai@aliyun.com>
2016-04-18 17:15:16 +0800

    a demo with flask

--- app/flasky.py
+++ app/flasky.py
@@ -1,5 +1,13 @@
 '''
-Created on 2016Äê4ÔÂ18ÈÕ
-
+Created on 2016-4-18
 @author: Administrator
 '''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+if __name__ == '__main__':
+    app.run(debug = True)
