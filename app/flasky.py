aa582cace254c27cbf93d3db756541231936ac3a
caosai <caosai@aliyun.com>
2016-04-18 22:32:40 +0800

    16-4-18 bootstrap failed

--- app/flasky.py
+++ app/flasky.py
@@ -3,18 +3,31 @@ Created on 2016-4-18
 @author: Administrator
 '''
 from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return 'hello world'
 
+@app.route('/redirect')
+def redir():
+    return redirect("http://baidu.com")
 
 @app.route('/user/<name>')
 def user(name):
-    return 'hello %s' % name
+    return render_template("user.html", name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
