d9a746883384aafd6b0c96a01c10345b274644ba
caosai <caosai@aliyun.com>
2016-04-19 17:12:56 +0800

    git webform

--- app/flasky.py
+++ app/flasky.py
@@ -6,13 +6,27 @@ from flask import Flask
 from flask import redirect
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import  StringField, SubmitField
+from wtforms.validators import Required
+from wsgiref.validate import validator
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+@app.route('/',methods = ['GET','POST'])
 def index():
-    return 'hello world'
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("index.html" , name=name,form=form,current_time=datetime.utcnow())
 
 @app.route('/redirect')
 def redir():
@@ -29,6 +43,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+
+class NameForm(Form):
+    name = StringField('What is your name?',validators = [Required()])
+    submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
