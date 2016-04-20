28e3551540536fd4fba1990ad91f93c3c343e596
caosai <caosai@aliyun.com>
2016-04-20 14:31:07 +0800

    sql

--- app/flasky.py
+++ app/flasky.py
@@ -2,8 +2,9 @@
 Created on 2016-4-18
 @author: Administrator
 '''
+import os
 from flask import Flask
-from flask import redirect
+from flask import redirect,session
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -11,22 +12,35 @@ from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import  StringField, SubmitField
 from wtforms.validators import Required
-from wsgiref.validate import validator
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.helpers import url_for
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
 
 @app.route('/',methods = ['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template("index.html" , name=name,form=form,current_time=datetime.utcnow())
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html" , name=session.get('name'),known = session.get('known',False),form=form,current_time=datetime.utcnow())
 
 @app.route('/redirect')
 def redir():
@@ -44,12 +58,29 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+    
+    def __repr__(self):
+        return '<Role  %r>' %self.name
 
-
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64),unique = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
 class NameForm(Form):
     name = StringField('What is your name?',validators = [Required()])
     submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug = True)
